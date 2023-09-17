2417db9d49b86aca34db50150b67ae4b683d15d2
Abdelrahman Modather <abdelrahmanmodather32@gmail.com>
2023-09-17 14:59:13 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,51 +1,61 @@
 #include "main.h"
 
 /**
-* _printf - the fun of alx project
-* @format: a var to be declared
-* Return: the wanted text
+* print_char - the fun prints char
+* @c: a char to be declared
+* Return: the wanted text (always 1)
 */
 
+static int print_char(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
 
+/**
+ * print_str - the fun prints string
+ * @strin: a string to be declared
+ * Return: Number of char printed
+ */
+static int print_str(const char *strin)
+{
+	int cont = 0;
+
+	while (*strin)
+	{
+		write(1, strin++, 1);
+		cont++;
+	}
+	return (cont);
+}
+
+/**
+ * _printf - the fun of alx project
+ * @format: a varible to be declared
+ * Return: the wanted text
+ */
 int _printf(const char *format, ...)
 {
-int intia_var = 0;
 va_list the_list;
-if (!format)
-return (-1);
+int intia_var = 0;
+
 va_start(the_list, format);
+
 while (*format)
 {
-if (*format != '%')
-{
-write(1, format, 1);
-intia_var++;
-}
-else
-{
-format++;
-if (*format == '\0')
-break;
-if (*format == 'c')
-{
-char c = va_arg(the_list, int);
-write(1, &c, 1);
-intia_var++;
-}
-else if (*format == 's')
-{
-char *new_str = va_arg(the_list, char*);
-int str = strlen(new_str);
-write(1, new_str, str);
-intia_var += str;
-}
-else if (*format == '%')
-{
-write(1, format, 1);
-intia_var++;
-}
-}
-format++;
+	if (*format != '%')
+		intia_var += print_char(*format);
+	else
+	{
+		format++;
+		if (*format == 'c')
+			intia_var += ptint_char(va_arg(the_list, int));
+		else if (*format == 's')
+			initia_var += print_str(va_arg(the_list, char *));
+		else if (*format == '%')
+			intia_var += print_char('%');
+	}
+	format++;
 }
 va_end(the_list);
 return (intia_var);
