6520ef14800a6531f2fcd488a1b5180c5738d83f
Abdelrahman Modather <abdelrahmanmodather32@gmail.com>
2023-09-18 10:14:38 -0700

    all

---
+++ testing/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int _printf(const char *format, ...);
+
+#endif
