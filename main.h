c0de0fc0e43673c9166c48eb493c868035dbe389
Abdelrahman Modather <138708453+Abdelrahman-Modather@users.noreply.github.com>
2023-09-18 03:55:30 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,15 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <string.h>
 #include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
-
-#define BUFF_SIZE 1024
 
 int _printf(const char *format, ...);
-
-
 
 #endif
