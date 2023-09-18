4223d5e098af95a5129020b29263df37d3332028
Abdelrahman Modather <abdelrahmanmodather32@gmail.com>
2023-09-17 18:53:53 -0700

    update

--- main.h
+++ main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-
 int _printf(const char *format, ...);
 int fputchar(int c, FILE *file);
 
