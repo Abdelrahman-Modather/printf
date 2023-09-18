c02ca1e882dcc8c0f8670e1e42e37e749786fc08
zoz01 <ziadgad2002@gmail.com>
2023-09-18 18:39:23 +0300

    jj

--- main.h
+++ main.h
@@ -7,10 +7,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define BUFF_SIZE 1024
 
 int _printf(const char *format, ...);
-int prnt_char(va_list the_list);
-int prnt_str(va_list the_list);
 
 #endif
