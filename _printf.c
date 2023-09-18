682ec8a3dd73a6319d1058290d04356337fcad02
Abdelrahman Modather <abdelrahmanmodather32@gmail.com>
2023-09-18 05:02:34 -0700

    UPDATE

--- _printf.c
+++ _printf.c
@@ -1,45 +1,88 @@
 #include "main.h"
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
 
 /**
-  * _printf - Printf function
-  * @format: format var
-  * Return: Printed text
-  */
-
-int _printf(const char *format, ...);
+ * _printf - the fun of alx project
+ * @format: the var to be declared
+ * ...: var arg
+ * Return: the wanted text
+ */
+int _printf(const char *format, ...)
 {
+	int iden = 0, t_c = 0;
 	va_list the_list;
-	int intia_var = 0;
 
+	if (!format)
+		return (-1);
 	va_start(the_list, format);
-
-	for (int i = 0; format[i] != '\0'; i++) {
-		if (format[i] != '%') {
-			putchar(format[i]);
-			intia_var++;
-		} else {
-			char spc = format[++i];
-
-			switch (spc) {
+	while (format[iden] != '\0')
+	{
+		if (format[iden] == '%')
+		{
+			iden++;
+			switch (format[iden])
+			{
+				case '%':
+					t_c += prnt_char(the_list);
+					break;
 				case 'c':
-					intia_var += putchar(va_arg(the_list, int));
+					t_c += prnt_char(the_list);
 					break;
 				case 's':
-					intia_var += puts(va_arg(the_list, char *));
-					break;
-				case '%':
-					putchar('%');
-					intia_vat++;
+					t_c += prnt_str(the_list);
 					break;
 				default:
-					putchar('%');
-					putchar(spc);
-					intia_var += 2;
+					if (write(STDOUT_FILENO, &format[iden - 1], 2) == -1)
+						return (-1);
+					t_c += 2;
+					break;
 			}
 		}
+		else
+		{
+			if (write(STDOUT_FILENO, &format[iden], 1) == -1)
+				return (-1);
+			t_c++;
+		}
+		i++;
 	}
-	
 	va_end(the_list);
+	return (t_c);
+}
+/**
+ * prnt_char - printing chars
+ * @the_list: list of args
+ * Return: total chars number
+ */
+int prnt_char(va_list the_list)
+{
+	char c = va_arg(the_list, int);
+	int mktob = write(STDOUT_FILENO, &c, 1);
 
-	return (intia_var);
+	if (mktob != 1)
+		return (-1);
+
+	return (1);
+}
+/**
+ * prnt_str - printing strings
+ * @the_list: list of args
+ * Return: total chars number
+ */
+int prnt_str(va_list the_list)
+{
+	char *streng = va_arg(the_list, char *);
+	int lngth, mktob;
+
+	if (!streng)
+		streng = "(null)";
+
+	lngth = strlen(streng);
+	mktob = write(STDOUT_FILENO, streng lngth);
+	if (mktob != lngth)
+		return (-1);
+
+	return (lngth);
 }
--- main.h
+++ main.h
@@ -5,9 +5,12 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define BUFF_SIZE 1024
 
 int _printf(const char *format, ...);
+int prnt_char(va_list the_list);
+int prnt_str(va_list the_list);
 
 #endif
