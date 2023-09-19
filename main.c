893964f9873db0d25efd0720a759b1d693d91b58
zoz01 <ziadgad2002@gmail.com>
2023-09-20 01:40:53 +0300

    yaa rab

--- main.c
+++ main.c
@@ -10,45 +10,44 @@
  */
 int main(void)
 {
-    int len;
+	int len;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    printf("%d\n", len);
-    len = printf("Let's try to printf a simple sentence.\n");
-    printf("%d\n", len);
-    len = _printf("empty Character:[%c]\n", ' ');
-    printf("%d\n", len);
-    len = printf("empty Character:[%c]\n", ' ');
-    len =_printf("Character:[%c]\n", 'H');
-    printf("%d\n", len);
-    len = printf("Character:[%c]\n", 'H');
-    len = _printf("Character:[%c%c]\n", 'H', 'I');
-    printf("%d\n", len);
-    len = printf("Character:[%c%c]\n", 'H', 'I');
-    len = _printf("Character:[%c%c%c]\n", 'H', 'I', '!');
-    printf("%d\n", len);
-    len = printf("Character:[%c%c%c]\n", 'H', 'I', '!');
+	len = _printf("Let's try to printf a simple sentence.\n");
+	printf("%d\n", len);
+	len = printf("Let's try to printf a simple sentence.\n");
+	printf("%d\n", len);
+	len = _printf("empty Character:[%c]\n", ' ');
+	printf("%d\n", len);
+	len = printf("empty Character:[%c]\n", ' ');
+	len = _printf("Character:[%c]\n", 'H');
+	printf("%d\n", len);
+	len = printf("Character:[%c]\n", 'H');
+	len = _printf("Character:[%c%c]\n", 'H', 'I');
+	printf("%d\n", len);
+	len = printf("Character:[%c%c]\n", 'H', 'I');
+	len = _printf("Character:[%c%c%c]\n", 'H', 'I', '!');
+	printf("%d\n", len);
+	len = printf("Character:[%c%c%c]\n", 'H', 'I', '!');
 
-    len = _printf("Character:[%c %c %c]\n", 'H', 'I', '!');
-    printf("%d\n", len);
-    len = printf("Character:[%c %c %c]\n", 'H', 'I', '!');
-    len = _printf("Two c with Space:[%c %c]\n", 'H', 'I');
-    printf("%d\n", len);
-    len = printf("Two c with Space:[%c %c]\n", 'H', 'I');
-    len = _printf("empty String:[%s]\n", "");
-    printf("%d\n", len);
-    len = printf("empty String:[%s]\n", "");
-    len = _printf("Space String:[%s]\n", " ");
-    printf("%d\n", len);
-    len = printf("Space String:[%s]\n", " ");
-    len = _printf("String:[%s]\n", "I am a string !");
-    printf("%d\n", len);
-    len = printf("String:[%s]\n", "I am a string !");
-    len = _printf("String:[%s%s]\n", "I am a string !", "second string !");
+	len = _printf("Character:[%c %c %c]\n", 'H', 'I', '!');
+	printf("%d\n", len);
+	len = printf("Character:[%c %c %c]\n", 'H', 'I', '!');
+	len = _printf("Two c with Space:[%c %c]\n", 'H', 'I');
+	printf("%d\n", len);
+	len = printf("Two c with Space:[%c %c]\n", 'H', 'I');
+	len = _printf("empty String:[%s]\n", "");
+	printf("%d\n", len);
+	len = printf("empty String:[%s]\n", "");
+	len = _printf("Space String:[%s]\n", " ");
+	printf("%d\n", len);
+	len = printf("Space String:[%s]\n", " ");
+	len = _printf("String:[%s]\n", "I am a string !");
+	printf("%d\n", len);
+	len = printf("String:[%s]\n", "I am a string !");
+	len = _printf("String:[%s%s]\n", "I am a string !", "second string !");
 
-   len = _printf("String:[%s%s]\n", "I am a string !", "second string !");
-   printf("%d\n", len);
-   len = printf("String:[%s %s]\n", "I am a string !", "second string !");
-
-    return (0);
+len = _printf("String:[%s%s]\n", "I am a string !", "second string !");
+printf("%d\n", len);
+len = printf("String:[%s %s]\n", "I am a string !", "second string !");
+return (0);
 }
