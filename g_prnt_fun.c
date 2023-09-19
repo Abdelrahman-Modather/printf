2e055cbe68cd574ca2b015fb3996729c90b146f4
zoz01 <ziadgad2002@gmail.com>
2023-09-19 23:29:22 +0300

    My first commit

--- g_prnt_fun.c
+++ g_prnt_fun.c
@@ -22,9 +22,9 @@ int (*g_prnt_fun(const char *i, int ndx))(va_list, char *, unsigned int)
 *{"+x", prnt_hex}, {"+X", prnt_uppx}, {" i", prnt_sint},
 *{" d", prnt_sint}, {" u", prnt_uint}, {" o", prnt_oct},
 *{" x", prnt_hex}, {" X", prnt_uppx}, {"R", prnt_root},
-*{"r", prnt_re},*/ {"%%", prnt_prs}, {" %%", prnt_prs},
+*{"r", prnt_re},*/ {"%", prnt_prs}, {" %", prnt_prs},
 /*
-*{"h", prnt_prs}, {" +i", prnt_pint},{"l", prnt_prs}, {" +d", prnt_pint},
+*{"h", prnt_prs},  {" +i", prnt_pint},{"l", prnt_prs}, {" +d", prnt_pint},
 *{"+ i", prnt_pint}, {"+ d", prnt_pint},
 *{NULL, NULL},
 */
