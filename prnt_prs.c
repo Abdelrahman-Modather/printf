139143569550f48fdf60c56a312e15536b16432e
zoz01 <ziadgad2002@gmail.com>
2023-09-19 21:19:38 +0300

    My first commit

--- prnt_prs.c
+++ prnt_prs.c
@@ -6,7 +6,7 @@
  * @a: a description for a
  * @bfi: a description for bfi
  * @a: a description for a
- * @buffp: pointer of buf
+ * @buffp: pointer of bufff
  * Return: on success 1
 */
 int prnt_prs(va_list a __attribute__((unused)), char *buffp, unsigned int bfi)
